1bb035bde586c8f72288646a4cddacbf8a68ea94
Silas Sefas <silassefas23@gmail.com>
2025-03-24 17:16:27 -0300

    Adicionando a exclusão do String no BoardColumn List

--- src/main/java/com/github/silassefas1/Board_De_Tarefas/persistence/entity/BoardEntity.java
+++ src/main/java/com/github/silassefas1/Board_De_Tarefas/persistence/entity/BoardEntity.java
@@ -1,6 +1,7 @@
 package com.github.silassefas1.Board_De_Tarefas.persistence.entity;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,8 @@ public class BoardEntity {
 
     private Long id;
     private String name;
+
+    @ToString.Exclude
     private List<BoardColumnEntity> boardColumn = new ArrayList<>();
 
 }
